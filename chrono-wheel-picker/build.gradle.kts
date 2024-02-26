10ea93185da6897a554db8bb9189657af2b8f221
darkokoa <mcdartz@gmail.com>
2024-02-27 00:31:44 +0800

    Upgrade libs versions & update gradle.properties config

--- chrono-wheel-picker/build.gradle.kts
+++ chrono-wheel-picker/build.gradle.kts
@@ -93,7 +93,7 @@ android {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.4"
+    kotlinCompilerExtensionVersion = "1.5.10"
   }
 }
 
--- sample/composeApp/build.gradle.kts
+++ sample/composeApp/build.gradle.kts
@@ -100,7 +100,7 @@ android {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.4"
+    kotlinCompilerExtensionVersion = "1.5.10"
   }
 }
 
