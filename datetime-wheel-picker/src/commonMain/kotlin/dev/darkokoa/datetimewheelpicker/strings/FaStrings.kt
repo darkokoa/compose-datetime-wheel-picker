10508273aed47fc9bd0e5fa45a92f94ad7b069c8
dkk <mcdartz@gmail.com>
2025-02-16 17:06:01 +0800

    Add Hebrew and Persian language support

---
+++ datetime-wheel-picker/src/commonMain/kotlin/dev/darkokoa/datetimewheelpicker/strings/FaStrings.kt
@@ -0,0 +1,46 @@
+package dev.darkokoa.datetimewheelpicker.strings
+
+import cafe.adriel.lyricist.LyricistStrings
+
+@LyricistStrings(languageTag = "fa")
+internal val FaStrings = Strings(
+  monthJanuaryFull = "ژانویه",
+  monthFebruaryFull = "فوریه",
+  monthMarchFull = "مارس",
+  monthAprilFull = "آوریل",
+  monthMayFull = "مه",
+  monthJuneFull = "ژوئن",
+  monthJulyFull = "ژوئیه",
+  monthAugustFull = "اوت",
+  monthSeptemberFull = "سپتامبر",
+  monthOctoberFull = "اکتبر",
+  monthNovemberFull = "نوامبر",
+  monthDecemberFull = "دسامبر",
+
+  monthJanuaryShort = "ژان",
+  monthFebruaryShort = "فور",
+  monthMarchShort = "مار",
+  monthAprilShort = "آور",
+  monthMayShort = "مه",
+  monthJuneShort = "ژون",
+  monthJulyShort = "ژوی",
+  monthAugustShort = "اوت",
+  monthSeptemberShort = "سپت",
+  monthOctoberShort = "اکت",
+  monthNovemberShort = "نوا",
+  monthDecemberShort = "دسا",
+
+  timeAM = "ق.ظ.",
+  timePM = "ب.ظ.",
+
+  digit0 = '٠',
+  digit1 = '١',
+  digit2 = '٢',
+  digit3 = '٣',
+  digit4 = '٤',
+  digit5 = '٥',
+  digit6 = '٦',
+  digit7 = '٧',
+  digit8 = '٨',
+  digit9 = '٩'
+)
---
+++ datetime-wheel-picker/src/commonMain/kotlin/dev/darkokoa/datetimewheelpicker/strings/HeStrings.kt
@@ -0,0 +1,35 @@
+package dev.darkokoa.datetimewheelpicker.strings
+
+import cafe.adriel.lyricist.LyricistStrings
+
+@LyricistStrings(languageTag = "he")
+internal val HeStrings = Strings(
+    monthJanuaryFull = "ינואר",
+    monthFebruaryFull = "פברואר",
+    monthMarchFull = "מרץ",
+    monthAprilFull = "אפריל",
+    monthMayFull = "מאי",
+    monthJuneFull = "יוני",
+    monthJulyFull = "יולי",
+    monthAugustFull = "אוגוסט",
+    monthSeptemberFull = "ספטמבר",
+    monthOctoberFull = "אוקטובר",
+    monthNovemberFull = "נובמבר",
+    monthDecemberFull = "דצמבר",
+
+    monthJanuaryShort = "ינו׳",
+    monthFebruaryShort = "פבר׳",
+    monthMarchShort = "מרץ",
+    monthAprilShort = "אפר׳",
+    monthMayShort = "מאי",
+    monthJuneShort = "יונ׳",
+    monthJulyShort = "יול׳",
+    monthAugustShort = "אוג׳",
+    monthSeptemberShort = "ספט׳",
+    monthOctoberShort = "אוק׳",
+    monthNovemberShort = "נוב׳",
+    monthDecemberShort = "דצמ׳",
+
+    timeAM = "AM",  
+    timePM = "PM"   
+)
