5cb728a90f4b804acc6a4bd47903abc78bfa6fd4
dkk <mcdartz@gmail.com>
2025-03-24 23:50:57 +0800

    Fix iOS sample build

--- sample/composeApp/src/iosMain/kotlin/main.kt
+++ sample/composeApp/src/iosMain/kotlin/main.kt
@@ -2,4 +2,7 @@ import androidx.compose.ui.window.ComposeUIViewController
 import dev.darkokoa.datetimewheelpicker.App
 import platform.UIKit.UIViewController
 
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController = ComposeUIViewController(
+  configure = { enforceStrictPlistSanityCheck = false },
+  content = { App() }
+)
